b5d0d0f213326f8ba565a0286ce533607c950a95
Ryan Whittingham <whittingham777@gmail.com>
2018-11-10 21:06:42 -0800

    Working getchar() to string out.

--- main.c
+++ main.c
@@ -3,5 +3,24 @@
 int main(){
 
 
+	struct phrase {
+		int phraseCount;
+		char phraseText[201];
+	};
+	
+	struct phrase dictionary[1000];
+	
+	int i=0, j=0, c=1;
 
-}
+	while (c != EOF){
+		c = getchar();
+		dictionary[j].phraseText[i]=c;
+		i++;	
+	}
+	
+	dictionary[j].phraseText[i-1]='\0';
+
+	printf("%s", dictionary[j].phraseText); 
+	
+	return 0;
+}	
