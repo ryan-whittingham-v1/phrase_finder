370edb1d24ece2d65ce0fd4d8225d7c393b2fde2
Ryan Whittingham <whittingham777@gmail.com>
2018-11-10 23:54:40 -0800

    Improve phrase formatting.

--- main.c
+++ main.c
@@ -1,4 +1,5 @@
 #include<stdio.h>
+#include<ctype.h>
 
 int main(){
 
@@ -10,17 +11,63 @@ int main(){
 	
 	struct phrase dictionary[1000];
 	
-	int i=0, j=0, c=1;
+	int i=0, j=0, c=1, k=0;
 
+	/////////////////    Loop through file  /////////////////
 	while (c != EOF){
+		
+		/////////////  get a character  ////////////////
 		c = getchar();
-		dictionary[j].phraseText[i]=c;
-		i++;	
+		////////////////////////////////////////////////
+
+		///////   change to uppercase  /////////////////
+		c = toupper(c);
+		///////////////////////////////////////////////
+	
+		///////////  Remove extra spaces  ////////////
+		if (isspace(c) > 0){
+			c = ' ';
+		}
+		///////////////////////////////////////////////
+		
+		////////////  ignore specific punctuation  ////////
+		/*if (c == '(' || c == ')' || c == '\'' || c == '\"' || c == '-'){
+			i++;	
+		}*/
+		////////////////////////////////////////////////
+		
+		///////////////   start of phrase  //////////////
+		if (i==0){
+			dictionary[j].phraseText[i]='<';
+			i++;
+			dictionary[j].phraseText[i]=c;
+		}
+		//////////////////////////////////////////////////
+
+		///////////////////  end of phrase  //////////////
+		else if (c == '.' || c == ',' || c== ';' || c == ':' || c == '?' || c == '!'){
+			c='>';
+			dictionary[j].phraseText[i]=c;
+			dictionary[j].phraseText[i+1]='\0';
+			j++;
+			i=0;
+		}
+		
+		else{
+			dictionary[j].phraseText[i]=c;
+			i++;
+		}
+		//////////////////////////////////////////////////
 	}
+	//////////////////////////////////////////////////////////
 	
 	dictionary[j].phraseText[i-1]='\0';
 
-	printf("%s", dictionary[j].phraseText); 
-	
+
+	/////////////////////  print phrases  ///////////////////
+	while(k < j){
+		printf("Phrase %i = %s\n", k, dictionary[k].phraseText); 
+		k++;
+	}
 	return 0;
 }	
